06fdab78be0a2d9897f09dbada5595556bb44174
ville <ville.prami@aalto.fi>
2023-08-11 19:15:46 +0300

    mobile scoreboard

--- src/components/Scoreboard.tsx
+++ src/components/Scoreboard.tsx
@@ -6,13 +6,23 @@ type ScoreboardProps = {
 
 const Scoreboard = ({ score }: ScoreboardProps) => {
   return (
-    <div className="inline-block border-4 border-headerOutline rounded-2xl px-4 py-4 mx-auto mt-10">
-      <div className="max-w-screen-sm w-screen flex justify-between">
-        <img src={logoBonus} className="ml-2" />
+    <div className="inline-block border-2 sm:border-4 border-headerOutline rounded-lg sm:rounded-2xl p-2 sm:p-4 mx-7 sm:mx-auto mt-8 sm:mt-10">
+      <div className="relative max-w-screen-sm sm:w-screen flex justify-between">
+        <div className="ml-3 flex sm:hidden items-center">
+          <img src={logoBonus} width={"45%"} />
+        </div>
 
-        <div className="bg-white rounded-lg text-center flex flex-col items-center justify-center px-12">
-          <h6 className="uppercase tracking-widest text-scoreText">score</h6>
-          <span className="text-5xl text-darkText">{score}</span>
+        <div className="ml-2 hidden sm:block">
+          <img src={logoBonus} width={"100%"} />
+        </div>
+
+        <div className="bg-white rounded-md sm:rounded-lg text-center flex flex-col items-center justify-center py-2 sm:py-0 px-6 sm:px-12">
+          <h6 className="uppercase tracking-widest text-scoreText text-[10px] sm:text-base">
+            score
+          </h6>
+          <span className="text-4xl sm:text-5xl font-bold text-darkText">
+            {score}
+          </span>
         </div>
       </div>
     </div>
